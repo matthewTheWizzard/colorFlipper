b7c8f6e1a2b2f89d7dc50eed803162e83efea00d
matthewTheWizzard <mmdrumnew@gmail.com>
2022-06-03 17:10:19 +0300

    simple task done

---
+++ main.js
@@ -0,0 +1,15 @@
+const colors = ['green', 'red', 'black', 'blue', 'orange', 'yellow', 'purple', 'cyan'];
+
+const button = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', function(event){
+    //get a random number between 0-2
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*colors.length);
+}
